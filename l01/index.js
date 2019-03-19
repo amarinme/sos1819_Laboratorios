2d2c015f9b4b2d321b7b415ee40aed5b9450bf91
sos1819amma <ana.marin.medina98@gmail.com>
2019-03-19 12:47:25 +0000

    API

---
+++ l01/index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+var moment = require("moment");
+
+var app = express();
+
+var puerto = process.env.PORT || 8080;
+
+app.get("/",(request, response) => {
+    var hora = moment().format('MMMM Do YYYY, h:mm:ss a');
+    response.send(hora);
+});
+
+app.listen(puerto);
+
+
