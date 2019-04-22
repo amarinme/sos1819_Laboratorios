85853b52fea1d10f38d2ea982eb8fdbf7ed8fd50
sos1819afg <adrianfergo93@gmail.com>
2019-04-22 15:06:04 +0000

    Refactor folders.

--- api/v1/happiness-stats/frontend_happiness_stats/public/main-ctrl.js
+++ api/v1/happiness-stats/frontend_happiness_stats/public/main-ctrl.js
@@ -6,16 +6,23 @@ angular
             console.log("Main Controller initialized.");
             var API = "/api/v1/happiness-stats";
        
+            refresh();
             
+            function refresh(){
+                
+                $http.get(API).then(function(response) {
+                        
+                        console.log("GET Request revived to " + "<" + API + ">");
+                        console.log("Data Received: " + JSON.stringify(response.data, null,2));
+                        $scope.happiness_stats = response.data;
+                        
+                    }
+                );
+                
+            }
             
-            console.log("Requesting happiness to <"+API+">...");
-            $http.get("/api/v1/happiness-stats").then(function(response){
-                console.log("Data Received: " + JSON.stringify(response.data, null,2));
-                
-                $scope.happiness_stats = response.data;
-                
-                });
-                $scope.addHappiness = function(){
+            //A PARTIR DE AQUI PUEDES PONER LOS BOTONES Y DEMÁS
+            $scope.addHappiness = function(){
                     var newHappiness = $scope.newHappiness;
                     console.log("Nuevo Ranking de felicidad");
                     $http.post(API ,newHappiness).then(function(response){
@@ -26,14 +33,16 @@ angular
                         });
                 };
                 
-                $scope.get = function(){
-                    $http.get(API).then(function(response){
-                        $scope.status = response.status;
-                        $scope.data = JSON.stringify(response.data,null,2);
-                    }, function (error){
-                        $scope.status = error.status;
-                        $scope.data = "";
-                        });
-                    };
+            $scope.get = function(){
+                $http.get(API).then(function(response){
+                    $scope.status = response.status;
+                    $scope.data = JSON.stringify(response.data,null,2);
+                }, function (error){
+                    $scope.status = error.status;
+                    $scope.data = "";
+                    });
+                };
+              
+            
         }]);
 
